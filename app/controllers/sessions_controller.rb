b2318859f0f7984d20564666cce754530d492c0e
Brant Wellman <BrantWellman@gmail.com>
2016-02-18 18:28:37 -0700

    User can log out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,10 @@ class SessionsController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def destroy
+    session.clear
+    flash[:notice] = {color: "white", message: "Goodbye"}
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "home#show"
   get "/dashboard", to: "users#show"
+  
   get "/auth/:provider/callback", to: 'sessions#create'
+  delete "/logout", to: "sessions#destroy"
 end
