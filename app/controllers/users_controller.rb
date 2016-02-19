c6b46aaafc4504531481e3605f59ae9164c154c7
Brant Wellman <BrantWellman@gmail.com>
2016-02-19 09:20:29 -0700

    Basic styling for dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
     @user_object = UserPresenter.new(current_user)
   end
 
+  def following
+    gs = GithubService.new(current_user)
+    @following_activity = gs.following_activity
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "home#show"
   get "/dashboard", to: "users#show"
-  
+  get "/following", to: "users#following"
+
   get "/auth/:provider/callback", to: 'sessions#create'
   delete "/logout", to: "sessions#destroy"
 end
